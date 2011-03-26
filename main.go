c32bbdd03df8737974ddb198eaa19c78a996da70
Will Walthall <ghthor@gmail.com>
2011-03-25 21:49:55 -0400

    Prepared to implement PushFutureEvent

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ type Event interface {
 
 // Type = [Busy, Idle]
 type State struct {
-    Type string
+    Status string
     TimeSpan
 }
 
@@ -67,10 +67,11 @@ type Sim struct {
     History *list.List
 }
 
-func (s Sim) SetNextState(state State) {
+func (s Sim) SetNextState(status string) {
+    s.State = s.State.Finalize()
     //Add the old state to the History
-    s.History.PushFront(s.State.Finalize())
-    s.State = state
+    s.History.PushFront(s.State)
+    s.State = State{status, TimeSpan{Began: s.State.Ended, Ended: s.State.Ended, TimeLine: s.TimeLine}}
 }
 
 type Staff struct {
@@ -80,6 +81,16 @@ type Staff struct {
 
 type Nurse Staff
 type Doctor Staff
+
+func (n Nurse) Proccess(p Patient) Event {
+    //TODO
+    return nil;
+}
+
+func (d Doctor) Proccess(p Patient) Event {
+    //TODO
+    return nil;
+}
 
 type Patient struct {
     Sim
@@ -96,18 +107,19 @@ func (e PatientArrives) Date() Date { return Date(e); }
 func (e PatientArrives) Execute(h *Hospital) {
     p := NewPatient(Date(e), h)
     h.WaitingRoom.AddPatient(p)
-    h.Patients[len(h.Patients)] = p
+    h.Patients = append(h.Patients, p)
 }
 
 //
 type NurseFreed struct {
-    date Date
-    Patient *Patient
     Nurse *Nurse
+    Patient *Patient
+    date Date
 }
 const NurseFreedEvent = "NurseFreed"
 func (e NurseFreed) Type() string { return NurseFreedEvent; }
 func (e NurseFreed) Date() Date { return e.date; }
+func (e NurseFreed) Execute(h *Hospital) {}
 
 //
 type DoctorFreed struct {
@@ -122,7 +134,7 @@ func (e DoctorFreed) Date() Date { return e.date; }
 //var events [...]string = ["Patient_Arrives", "Nurse_Idle", "Nurse_Process", "Doctor_Process", "Patient_Processed"]
 
 func NewPatient(arrivalDate Date, hospital *Hospital) *Patient {
-    p := &Patient{ Sim{ State{"Arriving", TimeSpan{ Began: arrivalDate}}, list.New()}, hospital, rand.Float64() * 60 + 1, rand.Intn(NUM_PRIORITIES)}
+    p := &Patient{ Sim{ State{"Arriving", TimeSpan{ Began: arrivalDate, TimeLine: hospital.TimeLine}}, list.New()}, hospital, rand.Float64() * 60 + 1, rand.Intn(NUM_PRIORITIES)}
     return p
 }
 
@@ -136,12 +148,19 @@ type TimeLine struct {
 }
 
 func (tl *TimeLine) ExecuteNextEvent() {
+    tl.Events.Remove(tl.Events.Front())
     e := tl.Events.Front().Value.(Event)
     tl.Now = e.Date()
+    //TODO
 }
 
-func (tl *TimeLine) PeekNextEvent() *Event {
+func (tl *TimeLine) PeekNextEvent() Event {
+    //TODO
     return nil
+}
+
+func (tl *TimeLine) PushFutureEvent(futureEvent Event) {
+    //TODO
 }
 
 //const WAITING_ROOM_Q_MAX_LENGTH = 300
@@ -169,8 +188,23 @@ func (w *WaitingRoom) AddPatient(p *Patient) {
     w.q[p.Priority].PushBack(p)
 }
 
-func (w *WaitingRoom) HasAvailablePatient(date Date) bool {
-    return false
+func (w *WaitingRoom) NextWaitingPatient(date Date) (nextWaiting *Patient) {
+    for i := NUM_PRIORITIES; i >= 0; i-- {
+        if w.q[i].Len() > 0 {
+            ele := w.q[i].Front()
+            p := ele.Value.(*Patient)
+            if p.Status == "WaitingForNurse" {
+                if p.DateOccursDuring(date) {
+                    w.q[i].Remove(ele)
+                    if nextWaiting != nil {
+                        w.q[i].PushFront(nextWaiting)
+                    }
+                    nextWaiting = p
+                }
+            }
+        }
+    }
+    return
 }
 
 type Hospital struct {
@@ -184,7 +218,7 @@ type Hospital struct {
 func NewHospital(spawnTime float64, nurses, doctors int) *Hospital {
     span := TimeSpan{0, 10*60, false, nil}
     totalPatients := int(math.Floor(float64(span.Ended)/spawnTime))
-    h := &Hospital{make([]*Patient, totalPatients), make([]*Nurse, nurses), make([]*Doctor, doctors), NewWaitingRoom(), &TimeLine{Events: list.New()}}
+    h := &Hospital{make([]*Patient, 0, totalPatients), make([]*Nurse, 0, nurses), make([]*Doctor, 0, doctors), NewWaitingRoom(), &TimeLine{Events: list.New()}}
 
     for i := 0; i < totalPatients; i++ {
         h.TimeLine.Events.PushBack(PatientArrives(float64(i)*spawnTime))
@@ -201,18 +235,23 @@ func (h *Hospital) ExecuteNextEvent() {
 }
 
 func (h *Hospital) CheckAndAssignNurse() {
-    if h.WaitingRoom.HasAvailablePatient(h.TimeLine.Now) {
-    }
+    now := h.TimeLine.Now
     idleNurses := make([]*Nurse, 0, len(h.Nurses))
     for _, nurse := range h.Nurses {
-        if nurse.Type == "Idle" {
+        if nurse.Status == "Idle" {
             idleNurses = append(idleNurses, nurse)
         }
     }
     if len(idleNurses) > 0 {
-        //nurse := idleNurses[rand.Intn(len(idleNurses))]
+        nurse := idleNurses[rand.Intn(len(idleNurses))]
         // Nurse Takes the Patient and generates the Lazy List Event
         //nurse.Sim.State
+        if p := h.WaitingRoom.NextWaitingPatient(now); p != nil {
+            nurse.SetNextState("Busy")
+            p.SetNextState("WithNurse")
+            futureEventDate := float64(now) + (p.ProcessingTime*.75)
+            h.TimeLine.PushFutureEvent(&NurseFreed{nurse, p, Date(futureEventDate)})
+        }
     }
 }
 
