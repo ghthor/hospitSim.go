bb4ff1a8b06333fd28bcfca49f095c069b99c6e2
Will Walthall <ghthor@gmail.com>
2011-03-26 14:12:48 -0400

    Adjusted the output Graph dimensions

--- assets/js/loadTable.js
+++ assets/js/loadTable.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-        $('table').visualize().wrapAll("<div class='centered'></dev>")
+        $('table').visualize({ width: 1200, height: 500, barGroupMargin: 2}).wrapAll("<div class='centered'></dev>")
         });
